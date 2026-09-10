f27d4e62ab1720e3772621d15f85a48d6043230c
Kushwaha-himanshu <kushwahahimanshu838@gmail.com>
2026-09-10 17:57:51 +0530

    updated_dbc connection

---
+++ backend/src/app.js
@@ -0,0 +1,29 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+const allowedOrigins = ["*",
+    "http://localhost:5173"
+];
+
+const app = express();
+
+app.use(
+    cors({
+        origin: function(origin, callback){
+            if(!origin || allowedOrigins.includes(origin)){
+                callback(null,true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials:true
+    })
+);
+//common middleware for acceptiong json formate file
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static("public"))
+// for parsing  cookies 
+app.use(cookieParser());
+// routes import
+export {app}
